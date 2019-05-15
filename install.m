eb4ebd296f2ffc1b2a8e20e4999b65e92749b2fc
YijYang <993413877@qq.com>
2019-05-15 12:40:46 +0800

    Update install.m

--- install.m
+++ install.m
@@ -22,30 +22,3 @@ if exist('external_libs/pdollar_toolbox/external', 'dir') == 7
 else
     warning('ECO:install', 'PDollars toolbox not found. Clone this submodule if you want to use HOG features. Skipping for now.')
 end
-
-% matconvnet
-if exist('external_libs/matconvnet/matlab', 'dir') == 7
-    cd external_libs/matconvnet/matlab
-    try
-        disp('Trying to compile MatConvNet with GPU support')
-        vl_compilenn('enableGpu', true)
-    catch err
-        warning('ECO:install', 'Could not compile MatConvNet with GPU support. Compiling for only CPU instead.\nVisit http://www.vlfeat.org/matconvnet/install/ for instructions of how to compile MatConvNet.\nNote: remember to move the mex-files after re-compiling.');
-        vl_compilenn;
-    end
-    status = movefile('mex/vl_*.mex*');
-    cd(home_dir)
-    
-    % donwload network
-    cd feature_extraction
-    mkdir networks
-    cd networks
-    if ~(exist('imagenet-vgg-m-2048.mat', 'file') == 2)
-        disp('Downloading the network "imagenet-vgg-m-2048.mat" from "http://www.vlfeat.org/matconvnet/models/imagenet-vgg-m-2048.mat"...')
-        urlwrite('http://www.vlfeat.org/matconvnet/models/imagenet-vgg-m-2048.mat', 'imagenet-vgg-m-2048.mat')
-        disp('Done!')
-    end
-    cd(home_dir)
-else
-    warning('ECO:install', 'Matconvnet not found. Clone this submodule if you want to use CNN features. Skipping for now.')
-end
